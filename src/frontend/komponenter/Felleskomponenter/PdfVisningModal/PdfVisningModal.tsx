c40f6ce36a5f7a1a42253698d7d05bd584f23866
Geir Roger Moen <geir.roger.moen@nav.no>
2021-05-05 10:59:01 +0200

    Enklere forhåndsvisning av vedtaksbrevet

--- src/frontend/komponenter/Felleskomponenter/PdfVisningModal/PdfVisningModal.tsx
+++ src/frontend/komponenter/Felleskomponenter/PdfVisningModal/PdfVisningModal.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 
-import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
 import styled from 'styled-components';
 
 import { AlertStripeFeil } from 'nav-frontend-alertstriper';
@@ -8,40 +7,28 @@ import Modal from 'nav-frontend-modal';
 import NavFrontendSpinner from 'nav-frontend-spinner';
 import { Undertittel } from 'nav-frontend-typografi';
 
-import { hentDataFraRessursMedFallback, Ressurs, RessursStatus } from '@navikt/familie-typer';
-
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import { Ressurs, RessursStatus } from '@navikt/familie-typer';
 
 const StyledModal = styled(Modal)`
     width: 50rem;
     height: 80%;
 
-    &__spinner {
-        align-items: center;
-        display: flex;
-        flex-direction: column;
-        padding-top: 5rem;
-
-        &--item {
-            margin-top: 2rem;
-            height: 7rem;
-            width: 7rem;
-        }
+    section {
+        height: 100%;
+        margin-right: 1.1rem;
     }
 
-    &__document {
-        &--feil {
-            top: 5rem;
-            position: relative;
-            margin: 0 1rem;
-        }
-
-        &--pages {
-            align-items: center;
-            display: flex;
-            flex-direction: column;
-        }
+    button {
+        margin-right: -0.7rem;
+        width: 1.5rem;
+        height: 1.5rem;
     }
+`;
+
+const IframePdfVisning = styled.iframe`
+    margin: 0 auto;
+    height: 100%;
+    width: 100%;
 `;
 
 interface IProps {
@@ -64,8 +51,6 @@ const PdfVisningModal: React.FC<IProps> = ({ onRequestClose, pdfdata, åpen }) =
 };
 
 const Dokument: React.FC<{ pdfdata: Ressurs<string> }> = ({ pdfdata }) => {
-    const [antallSider, settAntallSider] = React.useState<number>(0);
-
     switch (pdfdata.status) {
         case RessursStatus.HENTER:
             return (
@@ -75,38 +60,7 @@ const Dokument: React.FC<{ pdfdata: Ressurs<string> }> = ({ pdfdata }) => {
                 </div>
             );
         case RessursStatus.SUKSESS:
-            return (
-                <Document
-                    className={'pdfvisning-modal__dokument'}
-                    file={hentDataFraRessursMedFallback(pdfdata, undefined)}
-                    error={
-                        <AlertStripeFeil
-                            className={'pdfvisning-modal__document--feil'}
-                            children={'Ukjent feil ved henting av dokument.'}
-                        />
-                    }
-                    noData={
-                        <AlertStripeFeil
-                            className={'pdfvisning-modal__document--feil'}
-                            children={'Dokumentet er tomt.'}
-                        />
-                    }
-                    loading={<NavFrontendSpinner className={'skjemamodal__spinner--item'} />}
-                    onLoadSuccess={({ numPages }) => settAntallSider(numPages)}
-                >
-                    <div className={'pdfvisning-modal__document--pages'}>
-                        {antallSider > 0 &&
-                            [...Array(antallSider)].map((_: number, index: number) => {
-                                return (
-                                    <div key={index + 1}>
-                                        <Page pageNumber={index + 1} />
-                                        <hr />
-                                    </div>
-                                );
-                            })}
-                    </div>
-                </Document>
-            );
+            return <IframePdfVisning title={'Dokument'} src={pdfdata.data} />;
         case RessursStatus.FEILET:
         case RessursStatus.FUNKSJONELL_FEIL:
         case RessursStatus.IKKE_TILGANG:
