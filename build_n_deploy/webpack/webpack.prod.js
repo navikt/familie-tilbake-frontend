890e76c96bf85af5fbf38130bb0eddf3f5b61a27
Geir Roger Moen <geir.roger.moen@nav.no>
2021-09-22 15:41:07 +0200

    Source-maps i Sentry for feilsøking

--- build_n_deploy/webpack/webpack.prod.js
+++ build_n_deploy/webpack/webpack.prod.js
@@ -4,6 +4,7 @@ const merge = require('webpack-merge');
 const TerserPlugin = require('terser-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const SentryCliPlugin = require('@sentry/webpack-plugin');
 
 const baseConfig = require('./webpack.common');
 
@@ -22,12 +23,22 @@ const config = merge.mergeWithRules({
         filename: '[name].[contenthash].js',
         publicPath: '/assets/',
     },
+    devtool: 'source-map',
     plugins: [
         new CompressionPlugin({
             algorithm: 'gzip',
             test: /\.js$|\.css$|\.html$/,
             threshold: 10240,
             minRatio: 0.8,
+        }),
+        new SentryCliPlugin({
+            include: 'frontend_production',
+            org: 'nav',
+            project: 'familie-tilbake-frontend',
+            authToken: process.env.SENTRY_AUTH_TOKEN,
+            url: 'https://sentry.gc.nav.no/',
+            release: process.env.SENTRY_RELEASE,
+            urlPrefix: `~/assets`,
         }),
     ],
     optimization: {
