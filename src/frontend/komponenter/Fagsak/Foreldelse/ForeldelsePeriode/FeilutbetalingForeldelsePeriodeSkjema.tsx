68d00d1488ed6999aee401baf9e4255bd7d672ad
Geir Roger Moen <geir.roger.moen@nav.no>
2022-04-27 14:56:43 +0200

    Oppdaterte lenke til rutine

--- src/frontend/komponenter/Fagsak/Foreldelse/ForeldelsePeriode/FeilutbetalingForeldelsePeriodeSkjema.tsx
+++ src/frontend/komponenter/Fagsak/Foreldelse/ForeldelsePeriode/FeilutbetalingForeldelsePeriodeSkjema.tsx
@@ -88,7 +88,7 @@ const FeilutbetalingForeldelsePeriodeSkjema: React.FC<IProps> = ({
                         NAVI trenger tid for fristavbrytende tiltak. Husk å legge til nok tid ved
                         fastsettelse av frist. Se rutine for
                         <Link
-                            href="https://navno.sharepoint.com/sites/fag-og-ytelser-fagsystemer/SitePages/Foreldelse-av-tilbakebetalingskrav.aspx"
+                            href="https://navno.sharepoint.com/sites/TeamFamiliekopi/Shared%20Documents/Forms/AllItems.aspx?FolderCTID=0x012000AFBC2229208A6546861937F2075F148E&id=%2Fsites%2FTeamFamiliekopi%2FShared%20Documents%2FBarnetrygd%20%2D%20feilutbetaling%20og%20tilbakekreving%2FRutiner%20for%20tilbakekreving%2FForeldelse%20av%20tilbakebetalingskrav%2Epdf&parent=%2Fsites%2FTeamFamiliekopi%2FShared%20Documents%2FBarnetrygd%20%2D%20feilutbetaling%20og%20tilbakekreving%2FRutiner%20for%20tilbakekreving"
                             target="_blank"
                         >
                             foreldelse av tilbakebetalingskrav
@@ -110,7 +110,7 @@ const FeilutbetalingForeldelsePeriodeSkjema: React.FC<IProps> = ({
                     <BodyLong size="small" spacing>
                         Sett foreldelesfristen minimum 6 uker frem i tid. Se rutine for
                         <Link
-                            href="https://navno.sharepoint.com/sites/fag-og-ytelser-fagsystemer/SitePages/Foreldelse-av-tilbakebetalingskrav.aspx"
+                            href="https://navno.sharepoint.com/sites/TeamFamiliekopi/Shared%20Documents/Forms/AllItems.aspx?FolderCTID=0x012000AFBC2229208A6546861937F2075F148E&id=%2Fsites%2FTeamFamiliekopi%2FShared%20Documents%2FBarnetrygd%20%2D%20feilutbetaling%20og%20tilbakekreving%2FRutiner%20for%20tilbakekreving%2FForeldelse%20av%20tilbakebetalingskrav%2Epdf&parent=%2Fsites%2FTeamFamiliekopi%2FShared%20Documents%2FBarnetrygd%20%2D%20feilutbetaling%20og%20tilbakekreving%2FRutiner%20for%20tilbakekreving"
                             target="_blank"
                         >
                             foreldelse av tilbakebetalingskrav
--- src/frontend/komponenter/Felleskomponenter/FTHeader/FTHeader.tsx
+++ src/frontend/komponenter/Felleskomponenter/FTHeader/FTHeader.tsx
@@ -1,19 +1,13 @@
 import * as React from 'react';
 
-import { NavLink } from 'react-router-dom';
-
 import { Header } from '@navikt/familie-header';
 import { ISaksbehandler } from '@navikt/familie-typer';
-
-import { useApp } from '../../../context/AppContext';
 
 export interface IHeaderProps {
     innloggetSaksbehandler?: ISaksbehandler;
 }
 
 const FTHeader: React.FC<IHeaderProps> = ({ innloggetSaksbehandler }) => {
-    const { hentTilbakeInfo } = useApp();
-
     return (
         <Header
             tittel="NAV Familie - Tilbakekreving"
@@ -23,20 +17,7 @@ const FTHeader: React.FC<IHeaderProps> = ({ innloggetSaksbehandler }) => {
             }}
             brukerPopoverItems={[{ name: 'Logg ut', href: `${window.origin}/auth/logout` }]}
             eksterneLenker={[]}
-        >
-            <div>
-                <NavLink
-                    id="info"
-                    to="#"
-                    onClick={e => {
-                        hentTilbakeInfo();
-                        e.preventDefault();
-                    }}
-                >
-                    ?
-                </NavLink>
-            </div>
-        </Header>
+        />
     );
 };
 
