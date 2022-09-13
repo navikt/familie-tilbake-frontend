ee3f9e49feda7228d251f65b85a3c0e054e13789
Geir Roger Moen <geir.roger.moen@nav.no>
2022-09-13 15:17:58 +0200

    Disabler warnings til console under kjøring av test

--- src/jest-setup.ts
+++ src/jest-setup.ts
@@ -2,4 +2,14 @@ import '@testing-library/jest-dom';
 
 import { Crypto } from '@peculiar/webcrypto';
 
+global.console = {
+    ...console,
+    // uncomment to ignore a specific log level
+    log: jest.fn(),
+    debug: jest.fn(),
+    info: jest.fn(),
+    warn: jest.fn(),
+    // error: jest.fn(),
+};
+
 global.crypto = new Crypto();
