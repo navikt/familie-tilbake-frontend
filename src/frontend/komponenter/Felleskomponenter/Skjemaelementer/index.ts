410b8614b67803457e7aab6d1e32579ece317a50
marius-nav <Marius.Glad.Fredriksen@nav.no>
2023-11-29 08:22:51 +0100

    Oppdatert styled-components dependency fra 5.3.11 til 6.1.1 * Fix for HorisontalFamilieRadioGruppe marginbottom prop

--- src/frontend/komponenter/Felleskomponenter/Skjemaelementer/index.ts
+++ src/frontend/komponenter/Felleskomponenter/Skjemaelementer/index.ts
@@ -8,9 +8,8 @@ export * from './FixedDatovelger';
 export * from './FTDatovelger';
 export * from './LabelMedSpråk';
 
-export const HorisontalFamilieRadioGruppe = styled(FamilieRadioGruppe)`
-    margin-bottom: ${(props: { marginbottom?: string }) =>
-        props.marginbottom ? props.marginbottom : '12px'};
+export const HorisontalFamilieRadioGruppe = styled(FamilieRadioGruppe)<{ marginbottom?: string }>`
+    margin-bottom: ${({ marginbottom }) => (marginbottom ? marginbottom : '12px')};
 
     .navds-radio {
         display: inline-block;
