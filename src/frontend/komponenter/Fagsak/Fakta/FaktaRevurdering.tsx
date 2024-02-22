dbd1f8f604dfec7ecabf879f978fa705660afecb
Ida Merete Enholm <ida.merete.enholm@nav.no>
2024-02-22 13:42:27 +0100

    fjern bruk av row + columns for fakta-revurdering

--- src/frontend/komponenter/Fagsak/Fakta/FaktaRevurdering.tsx
+++ src/frontend/komponenter/Fagsak/Fakta/FaktaRevurdering.tsx
@@ -2,16 +2,14 @@ import * as React from 'react';
 
 import { styled } from 'styled-components';
 
-import { Column, Row } from 'nav-frontend-grid';
-
-import { BodyShort, Heading } from '@navikt/ds-react';
+import { BodyShort, Heading, HGrid, VStack } from '@navikt/ds-react';
 import { ASpacing4 } from '@navikt/ds-tokens/dist/tokens';
 
 import { IFeilutbetalingFakta, tilbakekrevingsvalg } from '../../../typer/feilutbetalingtyper';
 import { formatterDatostring } from '../../../utils';
 import { DetailBold, Spacer20 } from '../../Felleskomponenter/Flytelementer';
 
-export const RadMedMargin = styled(Row)`
+export const HGridMedMargin = styled(HGrid)`
     margin-bottom: ${ASpacing4};
 `;
 
@@ -22,58 +20,52 @@ interface IProps {
 const FaktaRevurdering: React.FC<IProps> = ({ feilutbetalingFakta }) => {
     return feilutbetalingFakta ? (
         <>
-            <Row>
-                <Column xs="12">
-                    <Heading level="2" size="small">
-                        Revurdering
-                    </Heading>
-                </Column>
-            </Row>
+            <Heading level="2" size="small">
+                Revurdering
+            </Heading>
             <Spacer20 />
-            <RadMedMargin>
-                <Column xs="6">
+            <HGridMedMargin columns={2}>
+                <VStack>
                     <DetailBold>Årsak(er) til revurdering</DetailBold>
                     {feilutbetalingFakta.faktainfo?.revurderingsårsak && (
                         <BodyShort size="small">
                             {feilutbetalingFakta.faktainfo.revurderingsårsak}
                         </BodyShort>
                     )}
-                </Column>
-                <Column xs="6">
+                </VStack>
+                <VStack>
                     <DetailBold>Dato for revurderingsvedtak</DetailBold>
                     <BodyShort size="small">
                         {formatterDatostring(feilutbetalingFakta.revurderingsvedtaksdato)}
                     </BodyShort>
-                </Column>
-            </RadMedMargin>
-            <RadMedMargin>
-                <Column xs="6">
+                </VStack>
+            </HGridMedMargin>
+            <HGridMedMargin columns={2}>
+                <VStack>
                     <DetailBold>Resultat</DetailBold>
                     {feilutbetalingFakta.faktainfo?.revurderingsresultat && (
                         <BodyShort size="small">
                             {feilutbetalingFakta.faktainfo.revurderingsresultat}
                         </BodyShort>
                     )}
-                </Column>
-                <Column xs="6">
+                </VStack>
+                <VStack>
                     <DetailBold>Konsekvens</DetailBold>
                     {feilutbetalingFakta.faktainfo?.konsekvensForYtelser && (
                         <BodyShort size="small">
                             {feilutbetalingFakta.faktainfo.konsekvensForYtelser?.join(', ')}
                         </BodyShort>
                     )}
-                </Column>
-            </RadMedMargin>
-            <Row>
-                <Column xs="6">
-                    <DetailBold>Tilbakekrevingsvalg</DetailBold>
-                    {feilutbetalingFakta.faktainfo?.tilbakekrevingsvalg && (
-                        <BodyShort size="small">
-                            {tilbakekrevingsvalg[feilutbetalingFakta.faktainfo.tilbakekrevingsvalg]}
-                        </BodyShort>
-                    )}
-                </Column>
-            </Row>
+                </VStack>
+            </HGridMedMargin>
+            <VStack>
+                <DetailBold>Tilbakekrevingsvalg</DetailBold>
+                {feilutbetalingFakta.faktainfo?.tilbakekrevingsvalg && (
+                    <BodyShort size="small">
+                        {tilbakekrevingsvalg[feilutbetalingFakta.faktainfo.tilbakekrevingsvalg]}
+                    </BodyShort>
+                )}
+            </VStack>
         </>
     ) : null;
 };
