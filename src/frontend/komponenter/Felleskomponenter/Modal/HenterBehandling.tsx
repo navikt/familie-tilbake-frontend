e781362232e21d2082f4c5324a1a0ae6fc8084c8
Kristine Steine <kristine.steine@bekk.no>
2024-03-05 12:28:41 +0100

    Fjern unødvendig fragment og løs styling med styled-components

--- src/frontend/komponenter/Felleskomponenter/Modal/HenterBehandling.tsx
+++ src/frontend/komponenter/Felleskomponenter/Modal/HenterBehandling.tsx
@@ -1,37 +1,35 @@
 import * as React from 'react';
 
+import styled from 'styled-components';
+
 import { BodyShort, Box, HStack, Heading, Loader, VStack } from '@navikt/ds-react';
+
+const BoxMedMargin = styled(Box)`
+    margin-top: 2rem;
+`;
 
 const HenterBehandling: React.FC = () => {
     return (
-        <>
-            <VStack justify="center" align="center">
-                <Box
-                    background="surface-subtle"
-                    padding="8"
-                    borderRadius="large"
-                    borderColor="border-default"
-                    style={{ marginTop: '2rem' }}
-                >
-                    <Heading level="1" size="medium">
-                        Henter behandling
-                    </Heading>
+        <VStack justify="center" align="center">
+            <BoxMedMargin
+                background="surface-subtle"
+                padding="8"
+                borderRadius="large"
+                borderColor="border-default"
+            >
+                <Heading level="1" size="medium">
+                    Henter behandling
+                </Heading>
 
-                    <div>
-                        <BodyShort>Henting av behandlingen tar litt tid.</BodyShort>
-                        <BodyShort>Vennligst vent!</BodyShort>
-                    </div>
-                    <HStack justify="center">
-                        <Loader
-                            size="large"
-                            title="venter..."
-                            transparent={false}
-                            variant="neutral"
-                        />
-                    </HStack>
-                </Box>
-            </VStack>
-        </>
+                <div>
+                    <BodyShort>Henting av behandlingen tar litt tid.</BodyShort>
+                    <BodyShort>Vennligst vent!</BodyShort>
+                </div>
+                <HStack justify="center">
+                    <Loader size="large" title="venter..." transparent={false} variant="neutral" />
+                </HStack>
+            </BoxMedMargin>
+        </VStack>
     );
 };
 
