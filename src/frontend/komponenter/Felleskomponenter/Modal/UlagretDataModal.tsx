4dbeca0568024d5b95bcc0259d05b5fde5520ce6
Viktor Solberg <viktor.grondalen.solberg@nav.no>
2024-05-07 10:56:27 +0200

    legger til to komponenter: ModalWrapper og UlagretDataModal

---
+++ src/frontend/komponenter/Felleskomponenter/Modal/UlagretDataModal.tsx
@@ -0,0 +1,39 @@
+import React, { FC } from 'react';
+
+import ReactRouterPrompt from 'react-router-prompt';
+
+import { ModalWrapper } from './ModalWrapper';
+import { useBehandling } from '../../../context/BehandlingContext';
+
+const UlagretDataModal: FC = () => {
+    const { nullstillIkkePersisterteKomponenter, harUlagredeData } = useBehandling();
+    return (
+        <ReactRouterPrompt when={harUlagredeData}>
+            {({ isActive, onConfirm, onCancel }) => (
+                <ModalWrapper
+                    tittel={
+                        'Du har ikke lagret dine siste endringer og vil miste disse om du forlater siden'
+                    }
+                    visModal={isActive}
+                    onClose={onCancel}
+                    aksjonsknapper={{
+                        hovedKnapp: {
+                            onClick: onCancel,
+                            tekst: 'Gå tilbake for å lagre',
+                        },
+                        lukkKnapp: {
+                            onClick: () => {
+                                onConfirm();
+                                setTimeout(nullstillIkkePersisterteKomponenter, 10);
+                            },
+                            tekst: 'Forlat siden',
+                        },
+                        marginTop: 4,
+                    }}
+                />
+            )}
+        </ReactRouterPrompt>
+    );
+};
+
+export default UlagretDataModal;
