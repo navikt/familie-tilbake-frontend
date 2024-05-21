0906b4ed03ee03fdb23a75231bb3a9df0a710bd0
charliemidtlyng <charlie.midtlyng@bekk.no>
2024-05-21 09:21:09 +0200

    Skal tilby *.ansatt.dev.nav.no som url for at teammedlemmer uten naisdevice skal kunne nå løsningen i preprod

--- src/backend/router.ts
+++ src/backend/router.ts
@@ -1,12 +1,25 @@
 import path from 'path';
 
-import { Response, Request, Router } from 'express';
+import { Response, Request, Router, NextFunction } from 'express';
 
 import { Client, ensureAuthenticated, envVar, logRequest } from '@navikt/familie-backend';
 import { LOG_LEVEL } from '@navikt/familie-logging';
 
 import { buildPath } from './config';
 import { prometheusTellere } from './metrikker';
+
+export const redirectHvisInternUrlIPreprod = () => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        if (
+            process.env.ENV === 'preprod' &&
+            req.headers.host === 'familie-tilbake-frontend.ansatt.dev.nav.no'
+        ) {
+            res.redirect(`https://familie-tilbake-frontend.ansatt.dev.nav.no${req.url}`);
+        } else {
+            next();
+        }
+    };
+};
 
 export default (authClient: Client, router: Router) => {
     router.get('/version', (_: Request, res: Response) => {
@@ -27,11 +40,16 @@ export default (authClient: Client, router: Router) => {
     });
 
     // APP
-    router.get('*', ensureAuthenticated(authClient, false), (_: Request, res: Response) => {
-        prometheusTellere.appLoad.inc();
+    router.get(
+        '*',
+        redirectHvisInternUrlIPreprod(),
+        ensureAuthenticated(authClient, false),
+        (_: Request, res: Response) => {
+            prometheusTellere.appLoad.inc();
 
-        res.sendFile('index.html', { root: path.join(process.cwd(), buildPath) });
-    });
+            res.sendFile('index.html', { root: path.join(process.cwd(), buildPath) });
+        }
+    );
 
     return router;
 };
