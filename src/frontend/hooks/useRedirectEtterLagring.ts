ac5bae0e70792a86cc87184b74287ec1ba9e879b
Charlie Midtlyng <charlie.midtlyng@bekk.no>
2024-12-19 10:30:11 +0100

    Ønsker å fjerne avhengigheter til react router prompt for å kunne opp… (#2018)
    * Ønsker å fjerne avhengigheter til react router prompt for å kunne oppgradere til react 19 - og samtidig er ikke behovet for denne avhengigheten reell lenger
    
    * importer useEffect direkte, ikke bruk React. ved bruk

--- src/frontend/hooks/useRedirectEtterLagring.ts
+++ src/frontend/hooks/useRedirectEtterLagring.ts
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import { useBehandling } from '../context/BehandlingContext';
 
 /**
- * Denne trengs fordi vi bruker react-router-prompt for å sperre for å navigere når man har ulagret data.
+ * Denne trengs fordi vi bruker blocker i react-router-dom for å sperre for å navigere når man har ulagret data.
  * Den sperren gjør at vi heller ikke klarer å navigere videre etter lagring og nullstilling av ulagret data.
  * For å omgå sperren må vi vente på at ulagretData er satt til false samtidig som vi sier at vi ønsker å navigere videre.
  * Dette fungerer ikke inni komponenten, så vi må ha denne hooken for å omgå problemet.
