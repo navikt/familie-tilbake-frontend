69e0b6a5d9edc855675471142a99846578b48193
Kevin Sillerud <kevin.sillerud@gmail.com>
2025-01-20 14:42:50 +0100

    Konfigurer for deployment til nytt namespace

--- src/backend/config.ts
+++ src/backend/config.ts
@@ -16,7 +16,7 @@ const Environment = () => {
         return {
             buildPath: 'frontend_production',
             namespace: 'e2e',
-            proxyUrl: 'http://familie-tilbake:8030/api',
+            proxyUrl: 'http://tilbakekreving-backend:8030/api',
             baSakUrl: 'http://familie-ba-sak-frontend:8000',
             efSakUrl: 'http://familie-ef-sak-frontend:8000/ekstern',
             ksSakUrl: 'http://familie-ks-sak-frontend:8000',
@@ -25,7 +25,7 @@ const Environment = () => {
         return {
             buildPath: 'frontend_production',
             namespace: 'preprod',
-            proxyUrl: 'http://familie-tilbake/api',
+            proxyUrl: 'http://tilbakekreving-backend/api',
             baSakUrl: 'https://barnetrygd.ansatt.dev.nav.no',
             efSakUrl: 'https://ensligmorellerfar.ansatt.dev.nav.no/ekstern',
             ksSakUrl: 'https://kontantstotte.ansatt.dev.nav.no',
@@ -34,7 +34,7 @@ const Environment = () => {
         return {
             buildPath: 'frontend_development',
             namespace: 'local',
-            proxyUrl: 'https://familie-tilbake.intern.dev.nav.no/api',
+            proxyUrl: 'https://tilbakekreving-backend.intern.dev.nav.no/api',
             baSakUrl: 'https://barnetrygd.ansatt.dev.nav.no',
             efSakUrl: 'https://ensligmorellerfar.ansatt.dev.nav.no/ekstern',
             ksSakUrl: 'https://kontantstotte.ansatt.dev.nav.no',
@@ -44,7 +44,7 @@ const Environment = () => {
     return {
         buildPath: 'frontend_production',
         namespace: 'production',
-        proxyUrl: 'http://familie-tilbake/api',
+        proxyUrl: 'http://tilbakekreving-backend/api',
         baSakUrl: 'https://barnetrygd.intern.nav.no',
         efSakUrl: 'https://ensligmorellerfar.intern.nav.no/ekstern',
         ksSakUrl: 'https://kontantstotte.intern.nav.no',
@@ -54,7 +54,7 @@ const env = Environment();
 
 export const sessionConfig: ISessionKonfigurasjon = {
     cookieSecret: [`${process.env.COOKIE_KEY1}`, `${process.env.COOKIE_KEY2}`],
-    navn: 'familie-tilbake-v1',
+    navn: 'tilbakekreving-backend-v1',
     redisFullUrl: process.env.REDIS_URI_SESSIONS,
     redisBrukernavn: process.env.REDIS_USERNAME_SESSIONS,
     redisPassord: process.env.REDIS_PASSWORD_SESSIONS,
@@ -67,7 +67,7 @@ export const sessionConfig: ISessionKonfigurasjon = {
 };
 
 if (!process.env.TILBAKE_SCOPE) {
-    throw new Error('Scope mot familie-tilbake er ikke konfigurert');
+    throw new Error('Scope mot tilbakekreving-backend er ikke konfigurert');
 }
 
 export const oboTilbakeConfig: IApi = {
