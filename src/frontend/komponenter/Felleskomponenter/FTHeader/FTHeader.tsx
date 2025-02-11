afcb7b4e692482b3c763f97f41689aacb1302fd3
Fredrik Garseg Mørk <fredrik.1997@hotmail.com>
2025-02-11 16:59:49 +0100

    Byttet ut familie-header med Internal Header

--- src/frontend/komponenter/Felleskomponenter/FTHeader/FTHeader.tsx
+++ src/frontend/komponenter/Felleskomponenter/FTHeader/FTHeader.tsx
@@ -1,23 +1,43 @@
 import * as React from 'react';
 
-import { Header } from '@navikt/familie-header';
+import { styled } from 'styled-components';
 import { type ISaksbehandler } from '@navikt/familie-typer';
+import { Dropdown, InternalHeader, Link, Spacer } from '@navikt/ds-react';
+import { LeaveIcon } from '@navikt/aksel-icons';
 
 interface IHeaderProps {
     innloggetSaksbehandler?: ISaksbehandler;
 }
 
+const StyledLink = styled(Link)`
+    text-decoration: none;
+`;
+
 const FTHeader: React.FC<IHeaderProps> = ({ innloggetSaksbehandler }) => {
     return (
-        <Header
-            tittelHref={'/'}
-            tittel="Nav - Tilbakekreving"
-            brukerinfo={{
-                navn: innloggetSaksbehandler?.displayName || 'Ukjent',
-            }}
-            brukerPopoverItems={[{ name: 'Logg ut', href: `${window.origin}/auth/logout` }]}
-            eksterneLenker={[]}
-        />
+        <InternalHeader>
+            <StyledLink href="/" className="">
+                <InternalHeader.Title>Nav - Tilbakekreving</InternalHeader.Title>
+            </StyledLink>
+
+            <Spacer />
+
+            <Dropdown>
+                <InternalHeader.UserButton
+                    as={Dropdown.Toggle}
+                    name={innloggetSaksbehandler?.displayName || 'Ukjent'}
+                />
+                <Dropdown.Menu>
+                    <Dropdown.Menu.List>
+                        <Dropdown.Menu.List.Item>
+                            <Link href={`${window.origin}/auth/logout`}>
+                                Logg ut <Spacer /> <LeaveIcon aria-hidden fontSize="1.5rem" />
+                            </Link>
+                        </Dropdown.Menu.List.Item>
+                    </Dropdown.Menu.List>
+                </Dropdown.Menu>
+            </Dropdown>
+        </InternalHeader>
     );
 };
 
