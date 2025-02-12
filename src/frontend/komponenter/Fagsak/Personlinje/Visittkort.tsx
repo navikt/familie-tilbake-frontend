69d8015bbd603eaad1288b966c042e9154a032ba
Fredrik Garseg Mørk <fredrik.1997@hotmail.com>
2025-02-12 13:49:43 +0100

    Fjernet familie-visittkort pakke og lagt til koden i eget repo

---
+++ src/frontend/komponenter/Fagsak/Personlinje/Visittkort.tsx
@@ -0,0 +1,49 @@
+import { CopyButton, HStack, Label } from '@navikt/ds-react';
+import { IPerson } from '../../../typer/person';
+import styled from 'styled-components';
+import { ABorderStrong, ASpacing4 } from '@navikt/ds-tokens/dist/tokens';
+import React from 'react';
+import { FamilieIkonVelger } from './PersonIkon/FamilieIkonVelger';
+
+interface IVisittkortProps {
+    navn: IPerson['navn'];
+    ident: IPerson['personIdent'];
+    kjønn: IPerson['kjønn'];
+    alder: number;
+    children: React.ReactNode;
+}
+
+const StyledVisittkort = styled(HStack)`
+    ${`border-bottom: 1px solid ${ABorderStrong}`};
+    height: 3rem;
+    padding: ${`0 ${ASpacing4}`};
+`;
+
+const GrådigChildrenContainer = styled(HStack)`
+    flex: 1;
+`;
+
+const Visittkort = ({ navn, alder, kjønn, ident, children }: IVisittkortProps) => {
+    return (
+        <StyledVisittkort align="center" justify="space-between" gap="4">
+            <HStack align="center" gap="4">
+                <FamilieIkonVelger alder={alder} kjønn={kjønn} width={24} height={24} />
+
+                <Label size={'small'}>
+                    {navn} ({alder} år)
+                </Label>
+
+                <div>|</div>
+                <HStack align="center" gap="1">
+                    {ident}
+                    <CopyButton copyText={ident.replace(' ', '')} size={'small'} />
+                </HStack>
+            </HStack>
+            <GrådigChildrenContainer align="center" gap="4">
+                {children}
+            </GrådigChildrenContainer>
+        </StyledVisittkort>
+    );
+};
+
+export default Visittkort;
