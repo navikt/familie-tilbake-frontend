deed1c91ef9bd5501e8b92fe4fa1fe7422b3b43b
Fredrik Garseg Mørk <fredrik.1997@hotmail.com>
2025-03-31 13:31:17 +0200

    Fjerner webpack spesifikke endringer

--- src/webpack/webpack.dev.ts
+++ src/webpack/webpack.dev.ts
@@ -1,5 +1,3 @@
-import type { Module } from 'webpack';
-
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import path from 'path';
 import webpack from 'webpack';
@@ -34,66 +32,6 @@ const devConfig: webpack.Configuration = mergeWithRules({
                 },
             },
         ],
-    },
-    optimization: {
-        runtimeChunk: 'single',
-        moduleIds: 'deterministic',
-        splitChunks: {
-            chunks: 'all',
-            maxInitialRequests: Infinity,
-            minSize: 20000,
-            maxSize: 244000,
-            cacheGroups: {
-                dateFns: {
-                    test: /[\\/]node_modules[\\/]date-fns/,
-                    name: 'vendor.date-fns',
-                    chunks: 'all',
-                    priority: 20,
-                    enforce: true,
-                    minSize: 0,
-                    maxSize: Infinity,
-                    reuseExistingChunk: true,
-                },
-                navikt: {
-                    test: /[\\/]node_modules[\\/]@navikt[\\/](.*?)[\\/]/,
-                    name(module: Module) {
-                        const packageName = module.context?.match(
-                            /[\\/]node_modules[\\/]@navikt[\\/](.*?)[\\/]/
-                        )?.[1];
-                        return `vendor.navikt.${packageName}`;
-                    },
-                    chunks: 'all',
-                    priority: 15,
-                    enforce: true,
-                    reuseExistingChunk: true,
-                },
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name(module: Module) {
-                        const packageName = module.context?.match(
-                            /[\\/]node_modules[\\/](.*?)([\\/]|$)/
-                        )?.[1];
-                        if (!packageName) return 'vendors';
-
-                        if (packageName.startsWith('@sentry/')) {
-                            return 'vendor.sentry';
-                        }
-                        if (packageName === 'react' || packageName === 'react-dom') {
-                            return 'vendor.react';
-                        }
-
-                        return `vendor.${packageName.replace('@', '')}`;
-                    },
-                    priority: 10,
-                },
-                common: {
-                    name: 'common',
-                    minChunks: 2,
-                    priority: -10,
-                    reuseExistingChunk: true,
-                },
-            },
-        },
     },
 });
 
