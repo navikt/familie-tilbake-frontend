a1034293fce677e5016ac318d04ac46e926d96ed
Fredrik Garseg Mørk <fredrik.1997@hotmail.com>
2025-06-13 10:13:30 +0200

    Bruker variabel RessursStatus.Suksess

--- src/frontend/hooks/useSettBehandlingTilbakeTilFakta.test.ts
+++ src/frontend/hooks/useSettBehandlingTilbakeTilFakta.test.ts
@@ -14,7 +14,7 @@ jest.mock('@tanstack/react-query', () => {
         useMutation: jest.fn(({ mutationFn, onSuccess }) => {
             const mutateAsync = async (behandlingId: string) => {
                 const result = await mutationFn(behandlingId);
-                if (onSuccess && result.status === 'SUKSESS') {
+                if (onSuccess && result.status === RessursStatus.Suksess) {
                     await onSuccess(result);
                 }
                 return result;
