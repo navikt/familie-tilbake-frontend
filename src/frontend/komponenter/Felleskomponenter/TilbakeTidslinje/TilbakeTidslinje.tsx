1987c69c5fec6fe74e890a60ccfaee427e6dfcfa
Fredrik Garseg Mørk <fredrik.1997@hotmail.com>
2025-06-19 08:33:39 +0200

    Fargekoder per periodestatus

--- src/frontend/komponenter/Felleskomponenter/TilbakeTidslinje/TilbakeTidslinje.tsx
+++ src/frontend/komponenter/Felleskomponenter/TilbakeTidslinje/TilbakeTidslinje.tsx
@@ -1,5 +1,6 @@
 import type { TimelinePeriodProps } from '@navikt/ds-react';
 
+import { CheckmarkCircleIcon, PencilIcon, XMarkOctagonIcon } from '@navikt/aksel-icons';
 import { Timeline } from '@navikt/ds-react';
 import {
     AGreen200,
@@ -13,14 +14,19 @@ import {
     ARed300,
     ARed400,
     ARed500,
-    ARed600,
     ABorderStrong,
     ASpacing5,
+    AOrange800,
+    AGreen800,
+    ARed800,
+    ARed200,
 } from '@navikt/ds-tokens/dist/tokens';
-import { format } from 'date-fns';
+import classNames from 'classnames';
 import * as React from 'react';
 import { styled } from 'styled-components';
 import { v4 as uuidv4 } from 'uuid';
+
+import { dateTilIsoDatoStringEllerUndefined } from '../../../utils/dato';
 
 const TidslinjeContainer = styled.div`
     border: 1px solid ${ABorderStrong};
@@ -35,25 +41,37 @@ const TidslinjeContainer = styled.div`
         background-color: ${AGreen200};
         border-color: ${AGreen400};
 
+        &:hover {
+            background-color: ${AGreen300};
+        }
+
         &.aktivPeriode {
             background-color: ${AGreen300};
             box-shadow: 0 0 0 2px ${AGreen500};
         }
     }
 
-    & .navds-timeline__period--error {
-        background-color: ${ARed300};
-        border-color: ${ARed500};
+    & .navds-timeline__period--danger {
+        background-color: ${ARed200};
+        border-color: ${ARed400};
+
+        &:hover {
+            background-color: ${ARed300};
+        }
 
         &.aktivPeriode {
-            background-color: ${ARed400};
-            box-shadow: 0 0 0 2px ${ARed600};
+            background-color: ${ARed300};
+            box-shadow: 0 0 0 2px ${ARed500};
         }
     }
 
     & .navds-timeline__period--warning {
         background-color: ${AOrange200};
         border-color: ${AOrange400};
+
+        &:hover {
+            background-color: ${AOrange300};
+        }
 
         &.aktivPeriode {
             background-color: ${AOrange300};
@@ -73,20 +91,57 @@ const TilbakeTidslinje: React.FC<IProps> = ({ rader, onSelectPeriode }) => {
             <Timeline>
                 {rader.map(rad => (
                     <Timeline.Row label="" key={uuidv4()}>
-                        {rad.map(periode => (
-                            <Timeline.Period
-                                key={periode.id}
-                                start={periode.start}
-                                end={periode.end}
-                                status={periode.status}
-                                isActive={periode.isActive}
-                                className={periode.className}
-                                onClick={() => onSelectPeriode(periode)}
-                            >
-                                Periode fra {format(new Date(periode.start), 'dd-MM-yyyy')} til{' '}
-                                {format(new Date(periode.end), 'dd-MM-yyyy')}
-                            </Timeline.Period>
-                        ))}
+                        {rad.map(periode => {
+                            const handling =
+                                periode.status === 'success'
+                                    ? 'Behandlet'
+                                    : periode.status === 'warning'
+                                      ? 'Ubehandlet'
+                                      : 'Avvist';
+                            const varighetTekst = `${handling} periode fra ${dateTilIsoDatoStringEllerUndefined(new Date(periode.start))} til ${dateTilIsoDatoStringEllerUndefined(new Date(periode.end))}`;
+                            const ikon =
+                                periode.status === 'warning' ? (
+                                    <PencilIcon
+                                        title="a11y-title"
+                                        fontSize="1.5rem"
+                                        aria-hidden="true"
+                                        style={{ color: AOrange800 }}
+                                    />
+                                ) : periode.status === 'success' ? (
+                                    <CheckmarkCircleIcon
+                                        title="a11y-title"
+                                        fontSize="1.5rem"
+                                        aria-hidden="true"
+                                        style={{ color: AGreen800 }}
+                                    />
+                                ) : (
+                                    <XMarkOctagonIcon
+                                        title="a11y-title"
+                                        fontSize="1.5rem"
+                                        aria-hidden="true"
+                                        style={{ color: ARed800 }}
+                                    />
+                                );
+                            console.log('xxx', periode.status);
+                            return (
+                                <Timeline.Period
+                                    key={periode.id}
+                                    icon={ikon}
+                                    start={periode.start}
+                                    end={periode.end}
+                                    status={periode.status}
+                                    isActive={periode.isActive}
+                                    className={classNames(
+                                        periode.className,
+                                        periode.isActive && 'aktivPeriode'
+                                    )}
+                                    aria-label={`${periode.status} ${varighetTekst}`}
+                                    onClick={() => onSelectPeriode(periode)}
+                                >
+                                    {varighetTekst}
+                                </Timeline.Period>
+                            );
+                        })}
                     </Timeline.Row>
                 ))}
             </Timeline>
