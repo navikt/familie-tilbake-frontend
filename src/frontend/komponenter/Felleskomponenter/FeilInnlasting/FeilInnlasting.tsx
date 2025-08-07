f13b09e0d37ed4af14b9bdfec4205c12b210cc25
Fredrik Garseg Mørk <fredrik.1997@hotmail.com>
2025-08-07 19:01:29 +0200

    Feilinnlastingskomponent

---
+++ src/frontend/komponenter/Felleskomponenter/FeilInnlasting/FeilInnlasting.tsx
@@ -0,0 +1,46 @@
+import type { ComponentType } from 'react';
+
+import { Button, Heading, VStack } from '@navikt/ds-react';
+import * as React from 'react';
+
+interface Props {
+    komponentNavn: string;
+}
+const FeilInnlasting = ({ komponentNavn }: Props) => {
+    return (
+        <VStack gap="2" align="center" justify="center">
+            <Heading level="2" size="small" spacing className="mt-6">
+                Feil ved innlasting av {komponentNavn}
+            </Heading>
+            <p>Det oppstod en feil under innlasting av komponenten. Vennligst prøv igjen senere.</p>
+            <Button variant="secondary" onClick={() => window.location.reload()}>
+                Last siden på nytt
+            </Button>
+        </VStack>
+    );
+};
+
+export const lazyImportMedRetry = <T,>(
+    importFunksjon: () => Promise<{ default: ComponentType<T> }>,
+    komponentNavn: string
+) => {
+    return React.lazy(() => {
+        let retryCount = 0;
+        const maxRetries = 2;
+        const attemptImport = async (): Promise<{ default: ComponentType<T> }> => {
+            return importFunksjon().catch((error: Error) => {
+                if (retryCount < maxRetries) {
+                    retryCount++;
+                    console.log(`Retry ${retryCount}/${maxRetries} for ${komponentNavn}:`, error);
+                    return attemptImport();
+                }
+
+                return {
+                    default: () => <FeilInnlasting komponentNavn={komponentNavn} />,
+                };
+            });
+        };
+
+        return attemptImport();
+    });
+};
