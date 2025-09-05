a3527d0a5b4ce96d835288afda69a530ed2d3862
Fredrik Garseg Mørk <31205303+fredrikmork@users.noreply.github.com>
2025-09-05 14:42:58 +0200

    Manuell foreldelse informasjonsboks (#2369)
    * Manuell foreldelse informasjonsboks

--- src/frontend/komponenter/Fagsak/Foreldelse/ForeldelseContainer.tsx
+++ src/frontend/komponenter/Fagsak/Foreldelse/ForeldelseContainer.tsx
@@ -1,9 +1,7 @@
 import type { IBehandling } from '../../../typer/behandling';
 
 import { Alert, BodyLong, Button, Heading, Link, VStack } from '@navikt/ds-react';
-import { ASpacing3 } from '@navikt/ds-tokens/dist/tokens';
 import * as React from 'react';
-import { styled } from 'styled-components';
 
 import { useFeilutbetalingForeldelse } from './FeilutbetalingForeldelseContext';
 import FeilutbetalingForeldelsePerioder from './ForeldelsePeriode/FeilutbetalingForeldelsePerioder';
@@ -11,30 +9,15 @@ import { useBehandling } from '../../../context/BehandlingContext';
 import { RessursStatus } from '../../../typer/ressurs';
 import { finnDatoRelativtTilNå } from '../../../utils';
 import DataLastIkkeSuksess from '../../Felleskomponenter/Datalast/DataLastIkkeSuksess';
-import { Navigering, Spacer20 } from '../../Felleskomponenter/Flytelementer';
-import Steginformasjon from '../../Felleskomponenter/Steginformasjon/StegInformasjon';
+import { Navigering } from '../../Felleskomponenter/Flytelementer';
 
-const getDate = (): string => {
-    return finnDatoRelativtTilNå({ months: -30 });
-};
-
-const StyledForeldelse = styled.div`
-    padding: ${ASpacing3};
-`;
-
-interface IProps {
+interface Props {
     behandling: IBehandling;
 }
 
-const ForeldelseContainer: React.FC<IProps> = ({ behandling }) => {
-    const {
-        feilutbetalingForeldelse,
-        skjemaData,
-        stegErBehandlet,
-        erAutoutført,
-        gåTilNesteSteg,
-        gåTilForrigeSteg,
-    } = useFeilutbetalingForeldelse();
+const ForeldelseContainer: React.FC<Props> = ({ behandling }) => {
+    const { feilutbetalingForeldelse, skjemaData, erAutoutført, gåTilNesteSteg, gåTilForrigeSteg } =
+        useFeilutbetalingForeldelse();
     const { behandlingILesemodus } = useBehandling();
     const erLesevisning = !!behandlingILesemodus || !!erAutoutført;
 
@@ -45,35 +28,37 @@ const ForeldelseContainer: React.FC<IProps> = ({ behandling }) => {
                     Foreldelse
                 </Heading>
                 <Alert variant="success" className="min-w-80">
-                    <Heading spacing size="small" level="3">
-                        Perioden er ikke foreldet
-                    </Heading>
-                    <VStack gap="6">
-                        <BodyLong>
-                            Ingen perioder er foreldet etter foreldelsesloven{' '}
-                            <Link
-                                href="https://lovdata.no/dokument/NL/lov/1979-05-18-18/KAPITTEL_1#%C2%A72"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="text-text-action"
-                            >
-                                § 2
-                            </Link>{' '}
-                            og{' '}
-                            <Link
-                                href="https://lovdata.no/dokument/NL/lov/1979-05-18-18/KAPITTEL_1#%C2%A73"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="text-text-action"
-                            >
-                                § 3
-                            </Link>
-                            .
-                        </BodyLong>
-                        <BodyLong>
-                            Perioden blir automatisk vurdert dersom det er mer enn 6 måneder til den
-                            er foreldet.
-                        </BodyLong>
+                    <VStack gap="2">
+                        <Heading size="small" level="3">
+                            Perioden er ikke foreldet
+                        </Heading>
+                        <VStack gap="6">
+                            <BodyLong>
+                                Ingen perioder er foreldet etter foreldelsesloven{' '}
+                                <Link
+                                    href="https://lovdata.no/dokument/NL/lov/1979-05-18-18/KAPITTEL_1#%C2%A72"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="text-text-action"
+                                >
+                                    § 2
+                                </Link>{' '}
+                                og{' '}
+                                <Link
+                                    href="https://lovdata.no/dokument/NL/lov/1979-05-18-18/KAPITTEL_1#%C2%A73"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="text-text-action"
+                                >
+                                    § 3
+                                </Link>
+                                .
+                            </BodyLong>
+                            <BodyLong>
+                                Perioden blir automatisk vurdert dersom det er mer enn 6 måneder til
+                                den er foreldet.
+                            </BodyLong>
+                        </VStack>
                     </VStack>
                 </Alert>
                 <Navigering>
@@ -90,20 +75,41 @@ const ForeldelseContainer: React.FC<IProps> = ({ behandling }) => {
 
     if (feilutbetalingForeldelse?.status === RessursStatus.Suksess) {
         return (
-            <StyledForeldelse>
-                <Heading spacing size="small" level="2">
+            <VStack padding="4" gap="4">
+                <Heading size="small" level="2">
                     Foreldelse
                 </Heading>
-                {(!erLesevisning || stegErBehandlet) && (
-                    <>
-                        <Steginformasjon
-                            behandletSteg={stegErBehandlet}
-                            infotekst={`I dette steget må du vurdere foreldelse manuelt. Perioden før ${getDate()} kan være foreldet. Del opp perioden ved behov og
-                                fastsett foreldelse.`}
-                        />
-                        <Spacer20 />
-                    </>
-                )}
+                <Alert variant="info" className="min-w-80" contentMaxWidth={false}>
+                    <VStack gap="2">
+                        <Heading size="small" level="3">
+                            Perioden før {finnDatoRelativtTilNå({ months: -30 })} kan være foreldet
+                        </Heading>
+                        <BodyLong size="medium">
+                            Når den alminnelige foreldelsesfristen etter foreldelsesloven{' '}
+                            <Link
+                                href="https://lovdata.no/dokument/NL/lov/1979-05-18-18/KAPITTEL_1#%C2%A72"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="text-text-action"
+                            >
+                                § 2
+                            </Link>{' '}
+                            og{' '}
+                            <Link
+                                href="https://lovdata.no/dokument/NL/lov/1979-05-18-18/KAPITTEL_1#%C2%A73"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="text-text-action"
+                            >
+                                § 3
+                            </Link>{' '}
+                            på 3 år er utløpt eller nærmer seg{' '}
+                            <span className="text-nowrap">(6 måneder før)</span>, må foreldelse
+                            vurderes manuelt. Del opp perioden ved behov og begrunn vurderingen.
+                        </BodyLong>
+                    </VStack>
+                </Alert>
+
                 {skjemaData.length > 0 && (
                     <FeilutbetalingForeldelsePerioder
                         behandling={behandling}
@@ -111,7 +117,7 @@ const ForeldelseContainer: React.FC<IProps> = ({ behandling }) => {
                         erLesevisning={erLesevisning}
                     />
                 )}
-            </StyledForeldelse>
+            </VStack>
         );
     } else {
         return <DataLastIkkeSuksess ressurser={[feilutbetalingForeldelse]} />;
