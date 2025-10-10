2aa5833b5cf33e3eb62fe92265f496cd5995977c
Fredrik Garseg Mørk <fredrik.1997@hotmail.com>
2025-10-10 11:33:52 +0200

    Faktaboks tags og tester for faktaboksen

--- src/frontend/komponenter/Fagsak/Høyremeny/Informasjonsbokser/Faktaboks.tsx
+++ src/frontend/komponenter/Fagsak/Høyremeny/Informasjonsbokser/Faktaboks.tsx
@@ -1,5 +1,6 @@
 import type { Behandling } from '../../../../typer/behandling';
 import type { Fagsak } from '../../../../typer/fagsak';
+import type { TagProps } from '@navikt/ds-react';
 
 import {
     Buildings3Icon,
@@ -7,10 +8,23 @@ import {
     CalendarIcon,
     ClipboardIcon,
     FileCheckmarkIcon,
+    FileLoadingIcon,
+    FilePlusIcon,
+    FileResetIcon,
+    TasklistIcon,
+    TasklistSendIcon,
 } from '@navikt/aksel-icons';
 import { Box, Heading, Tag } from '@navikt/ds-react';
 import React from 'react';
 
+import { ytelsetype } from '../../../../kodeverk';
+import { Behandlingresultat } from '../../../../typer/behandling';
+import {
+    behandlingsresultater,
+    behandlingsstatuser,
+    behandlingårsaker,
+} from '../../../../typer/behandling';
+import { Behandlingstatus } from '../../../../typer/behandling';
 import { formatterDatostring } from '../../../../utils';
 
 type Props = {
@@ -19,73 +33,47 @@ type Props = {
 };
 
 export const Faktaboks: React.FC<Props> = ({ behandling, ytelsestype }) => {
-    // const årsak = ''; // TODO
-    const status = 'Avsluttet'; // TODO
-    const resultat = 'Ingen tilbakebetaling'; // TODO
-    const avsluttet = '01.01.2025'; // TODO
-    const enhet = '1234'; // TODO
-
     return (
         <Box
             padding="4"
             className="border-1 rounded-xl border-border-divider flex flex-col gap-4 bg-white"
         >
             <Heading size="xsmall" level="2">
-                Revurdering tilbakekreving av {ytelsestype}
+                Tilbakekreving av {ytelsetype[ytelsestype]}
             </Heading>
 
             <dl className="grid grid-cols-[136px_1fr] xl:grid-cols-[152px_1fr] gap-y-2 gap-x-4">
-                <dt className="text-medium font-bold flex flex-row gap-2 items-center">
-                    <FileCheckmarkIcon
-                        title="a11y-title"
-                        fontSize="1rem"
-                        className="text-icon-subtle"
-                    />
-                    Status
-                </dt>
-                <dd className="text-medium items-center flex">
-                    <Tag size="small" variant="success-moderate" className="border-0 px-2">
-                        {status}
-                        {/* TODO Chip */}
-                    </Tag>
-                </dd>
-
-                {resultat && (
+                {behandling.behandlingsårsakstype && (
                     <>
-                        <dt className="shrink-0 text-medium font-bold flex flex-row gap-2 items-center">
-                            <ClipboardIcon
+                        <dt className="text-medium font-bold flex flex-row gap-2 items-center">
+                            <FileResetIcon
                                 title="a11y-title"
                                 fontSize="1rem"
                                 className="text-icon-subtle"
                             />
-                            Resultat
+                            Revurderingsårsak
                         </dt>
                         <dd className="text-medium items-center flex">
-                            <Tag size="small" variant="warning-moderate" className="border-0 px-2">
-                                {resultat}
-                                {/* TODO Chip */}
+                            <Tag size="small" variant="neutral-moderate" className="border-0 px-2">
+                                {behandlingårsaker[behandling.behandlingsårsakstype]}
                             </Tag>
                         </dd>
                     </>
                 )}
 
-                {behandling.opprettetDato && (
-                    <>
-                        <dt className="text-medium font-bold flex flex-row gap-2 items-center">
-                            <CalendarIcon
-                                title="a11y-title"
-                                fontSize="1rem"
-                                className="text-icon-subtle"
-                            />
-                            Opprettet
-                        </dt>
-                        <dd className="text-medium items-center flex">
-                            {formatterDatostring(behandling.opprettetDato)}
-                        </dd>
-                    </>
-                )}
+                <StatusTag status={behandling.status} />
 
-                {avsluttet && (
+                {behandling.resultatstype && <ResultatTag resultat={behandling.resultatstype} />}
+
+                <dt className="text-medium font-bold flex flex-row gap-2 items-center">
+                    <CalendarIcon title="a11y-title" fontSize="1rem" className="text-icon-subtle" />
+                    Opprettet
+                </dt>
+                <dd className="text-medium items-center flex">
+                    {formatterDatostring(behandling.opprettetDato)}
+                </dd>
+
+                {behandling.avsluttetDato && (
                     <>
                         <dt className="text-medium font-bold flex flex-row gap-2 items-center">
                             <CalendarFillIcon
@@ -95,24 +83,101 @@ export const Faktaboks: React.FC<Props> = ({ behandling, ytelsestype }) => {
                             />
                             Avsluttet
                         </dt>
-                        <dd className="text-medium items-center flex">{avsluttet}</dd>
+                        <dd className="text-medium items-center flex">
+                            {formatterDatostring(behandling.avsluttetDato)}
+                        </dd>
                     </>
                 )}
 
-                {enhet && (
-                    <>
-                        <dt className="text-medium font-bold flex flex-row gap-2 items-center">
-                            <Buildings3Icon
-                                title="a11y-title"
-                                fontSize="1rem"
-                                className="text-icon-subtle"
-                            />
-                            Enhet
-                        </dt>
-                        <dd className="text-medium items-center flex">{enhet}</dd>
-                    </>
-                )}
+                <dt className="text-medium font-bold flex flex-row gap-2 items-center">
+                    <Buildings3Icon
+                        title="a11y-title"
+                        fontSize="1rem"
+                        className="text-icon-subtle"
+                    />
+                    Enhet
+                </dt>
+                <dd className="text-medium items-center flex">
+                    {behandling.enhetskode} {behandling.enhetsnavn}
+                </dd>
             </dl>
         </Box>
+    );
+};
+
+const STATUS_META = {
+    [Behandlingstatus.Opprettet]: {
+        variant: 'neutral-moderate',
+        icon: FilePlusIcon,
+    },
+    [Behandlingstatus.Utredes]: {
+        variant: 'info-moderate',
+        icon: FileLoadingIcon,
+    },
+    [Behandlingstatus.FatterVedtak]: {
+        variant: 'alt2-moderate',
+        icon: TasklistIcon,
+    },
+    [Behandlingstatus.IverksetterVedtak]: {
+        variant: 'info-moderate',
+        icon: TasklistSendIcon,
+    },
+    [Behandlingstatus.Avsluttet]: {
+        variant: 'success-moderate',
+        icon: FileCheckmarkIcon,
+    },
+} satisfies Record<
+    Behandlingstatus,
+    {
+        variant: TagProps['variant'];
+        icon: React.ComponentType;
+    }
+>;
+
+const StatusTag: React.FC<{ status: Behandlingstatus }> = ({ status }) => {
+    const { variant, icon: StatusIkon } = STATUS_META[status];
+    return (
+        <>
+            <dt className="text-medium font-bold flex flex-row gap-2 items-center">
+                <StatusIkon title="a11y-title" fontSize="1rem" className="text-icon-subtle" />
+                Status
+            </dt>
+            <dd className="text-medium items-center flex">
+                <Tag size="small" variant={variant} className="border-0 px-2">
+                    {behandlingsstatuser[status]}
+                </Tag>
+            </dd>
+        </>
+    );
+};
+
+const RESULTAT_META = {
+    [Behandlingresultat.Henlagt]: { variant: 'error-moderate' },
+    [Behandlingresultat.HenlagtFeilopprettet]: { variant: 'error-moderate' },
+    [Behandlingresultat.HenlagtFeilopprettetMedBrev]: { variant: 'error-moderate' },
+    [Behandlingresultat.HenlagtFeilopprettetUtenBrev]: { variant: 'error-moderate' },
+    [Behandlingresultat.IkkeFastsatt]: { variant: 'error-moderate' },
+    [Behandlingresultat.IngenTilbakebetaling]: { variant: 'warning-moderate' },
+    [Behandlingresultat.DelvisTilbakebetaling]: { variant: 'warning-moderate' },
+    [Behandlingresultat.FullTilbakebetaling]: { variant: 'info-moderate' },
+} satisfies Record<Behandlingresultat, { variant: TagProps['variant'] }>;
+
+const ResultatTag: React.FC<{ resultat: Behandlingresultat }> = ({ resultat }) => {
+    return (
+        <>
+            <dt className="shrink-0 text-medium font-bold flex flex-row gap-2 items-center">
+                <ClipboardIcon title="a11y-title" fontSize="1rem" className="text-icon-subtle" />
+                Resultat
+            </dt>
+            <dd className="text-medium items-center flex">
+                <Tag
+                    size="small"
+                    variant={RESULTAT_META[resultat].variant}
+                    className="border-0 px-2"
+                >
+                    {behandlingsresultater[resultat]}
+                </Tag>
+            </dd>
+        </>
     );
 };
