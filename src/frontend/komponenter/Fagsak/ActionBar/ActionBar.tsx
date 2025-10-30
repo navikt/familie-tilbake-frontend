655bfaf2d371e914132336dd85ee2d60af89f5ed
Fredrik Garseg Mørk <fredrik.1997@hotmail.com>
2025-10-30 15:43:51 +0100

    Minimumsbredde for action-bar

--- src/frontend/komponenter/Fagsak/ActionBar/ActionBar.tsx
+++ src/frontend/komponenter/Fagsak/ActionBar/ActionBar.tsx
@@ -34,7 +34,7 @@ const ActionBar: React.FC<Props> = ({
         <HStack
             /* Hacker plasseringen til pga at den ikke er på behandlingcontainer nivå men nede i stegcontainerene som styrer hvor startposisjon er */
             className={classNames(
-                'fixed bottom-4 bg-ax-bg-default px-8 py-4 rounded-2xl border-ax-border-neutral-subtle border justify-between z-10 flex-nowrap ax-lg:w-[calc((100vw*2/3)-34px)] w-[calc(100vw-32px)]',
+                'fixed bottom-4 bg-ax-bg-default px-8 py-4 rounded-2xl border-ax-border-neutral-subtle border justify-between z-10 flex-nowrap ax-lg:w-[calc((100vw*2/3)-34px)] w-[calc(100vw-32px)] min-w-96',
                 {
                     '-ml-6': !dobbeltNøstet,
                     '-ml-10': dobbeltNøstet,
