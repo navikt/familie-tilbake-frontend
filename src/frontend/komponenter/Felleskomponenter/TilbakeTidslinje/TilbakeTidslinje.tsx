42a1af9a017de4f1ef245fa8149164f1c6039ecd
Fredrik Garseg Mørk <31205303+fredrikmork@users.noreply.github.com>
2025-11-04 13:12:06 +0100

    Navds elementer (#2480)
    * Endret fra navds til aksel etter darkside innføringen
    
    * Gjorde tidslinjen default til aksel igjen

--- src/frontend/komponenter/Felleskomponenter/TilbakeTidslinje/TilbakeTidslinje.tsx
+++ src/frontend/komponenter/Felleskomponenter/TilbakeTidslinje/TilbakeTidslinje.tsx
@@ -2,24 +2,7 @@ import type { TimelinePeriodProps } from '@navikt/ds-react';
 
 import { CheckmarkCircleIcon, PencilIcon, XMarkOctagonIcon } from '@navikt/aksel-icons';
 import { Timeline } from '@navikt/ds-react';
-import {
-    AGreen200,
-    AGreen300,
-    AGreen400,
-    AGreen500,
-    AOrange200,
-    AOrange300,
-    AOrange400,
-    AOrange500,
-    ARed300,
-    ARed400,
-    ARed500,
-    ASpacing5,
-    AOrange800,
-    AGreen800,
-    ARed800,
-    ARed200,
-} from '@navikt/ds-tokens/dist/tokens';
+import { ASpacing5 } from '@navikt/ds-tokens/dist/tokens';
 import classNames from 'classnames';
 import * as React from 'react';
 import { styled } from 'styled-components';
@@ -32,48 +15,6 @@ const TidslinjeContainer = styled.div`
 
     .etiketter div:last-child {
         max-width: max-content;
-    }
-
-    & .navds-timeline__period--success {
-        background-color: ${AGreen200};
-        border-color: ${AGreen400};
-
-        &:hover {
-            background-color: ${AGreen300};
-        }
-
-        &.aktivPeriode {
-            background-color: ${AGreen300};
-            box-shadow: 0 0 0 2px ${AGreen500};
-        }
-    }
-
-    & .navds-timeline__period--danger {
-        background-color: ${ARed200};
-        border-color: ${ARed400};
-
-        &:hover {
-            background-color: ${ARed300};
-        }
-
-        &.aktivPeriode {
-            background-color: ${ARed300};
-            box-shadow: 0 0 0 2px ${ARed500};
-        }
-    }
-
-    & .navds-timeline__period--warning {
-        background-color: ${AOrange200};
-        border-color: ${AOrange400};
-
-        &:hover {
-            background-color: ${AOrange300};
-        }
-
-        &.aktivPeriode {
-            background-color: ${AOrange300};
-            box-shadow: 0 0 0 2px ${AOrange500};
-        }
     }
 `;
 
@@ -98,26 +39,11 @@ const TilbakeTidslinje: React.FC<Props> = ({ rader, onSelectPeriode }) => {
                             const varighetTekst = `${handling} periode fra ${dateTilIsoDatoStringEllerUndefined(new Date(periode.start))} til ${dateTilIsoDatoStringEllerUndefined(new Date(periode.end))}`;
                             const ikon =
                                 periode.status === 'warning' ? (
-                                    <PencilIcon
-                                        title="a11y-title"
-                                        fontSize="1.5rem"
-                                        aria-hidden="true"
-                                        style={{ color: AOrange800 }}
-                                    />
+                                    <PencilIcon aria-hidden />
                                 ) : periode.status === 'success' ? (
-                                    <CheckmarkCircleIcon
-                                        title="a11y-title"
-                                        fontSize="1.5rem"
-                                        aria-hidden="true"
-                                        style={{ color: AGreen800 }}
-                                    />
+                                    <CheckmarkCircleIcon aria-hidden />
                                 ) : (
-                                    <XMarkOctagonIcon
-                                        title="a11y-title"
-                                        fontSize="1.5rem"
-                                        aria-hidden="true"
-                                        style={{ color: ARed800 }}
-                                    />
+                                    <XMarkOctagonIcon aria-hidden />
                                 );
 
                             return (
