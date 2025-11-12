35f2b946df7f69503bfbc08672f6585cc5a54aab
Fredrik Garseg Mørk <fredrik.1997@hotmail.com>
2025-11-12 13:32:10 +0100

    La til kontrakter i openapi genereringen

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -1,12 +1,22 @@
 import { defineConfig } from '@hey-api/openapi-ts';
 
 export default defineConfig({
-    input: 'https://tilbakekreving-backend.intern.dev.nav.no/v3/api-docs',
-    output: {
-        path: 'src/frontend/generated',
-        format: 'prettier',
-        lint: 'eslint',
-    },
+    input: [
+        'https://tilbakekreving-backend.intern.dev.nav.no/v3/api-docs',
+        'https://raw.githubusercontent.com/navikt/tilbakekreving-kontrakter/refs/heads/main/tsp-output/schema/openapi.yaml',
+    ],
+    output: [
+        {
+            path: 'src/frontend/generated',
+            format: 'prettier',
+            lint: 'eslint',
+        },
+        {
+            path: 'src/frontend/kontrakter',
+            format: 'prettier',
+            lint: 'eslint',
+        },
+    ],
     parser: {
         transforms: {
             enums: 'root',
