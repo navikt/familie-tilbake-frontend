85ebc7dd797380c48cb45dfb76d0b641c8a486ae
Camilla Kaasi <camilla.lauritzen.kaasi@nav.no>
2025-11-21 11:12:06 +0100

    justerer dato funksjon

--- src/frontend/utils/dateUtils.ts
+++ src/frontend/utils/dateUtils.ts
@@ -163,8 +163,11 @@ export const formatterRelativTid = (dato: string): string => {
 
     const ukerSiden = differenceInWeeks(now, target);
     const dagerSiden = differenceInDays(now, target);
+    const månederSiden = differenceInMonths(now, target);
 
-    if (ukerSiden >= 1) {
+    if (ukerSiden >= 4) {
+        return `${månederSiden} ${månederSiden === 1 ? 'måned' : 'måneder'} siden`;
+    } else if (ukerSiden >= 3) {
         return `${ukerSiden} ${ukerSiden === 1 ? 'uke' : 'uker'} siden`;
     } else if (dagerSiden >= 1) {
         return `${dagerSiden} ${dagerSiden === 1 ? 'dag' : 'dager'} siden`;
