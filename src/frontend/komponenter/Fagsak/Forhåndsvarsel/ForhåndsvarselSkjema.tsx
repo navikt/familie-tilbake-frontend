fea97e92ad5bc626a753831b2147c32655e668ce
Camilla Kaasi <camilla.lauritzen.kaasi@nav.no>
2025-11-21 11:12:35 +0100

    legger til støtte for forhåndsvisning

--- src/frontend/komponenter/Fagsak/Forhåndsvarsel/ForhåndsvarselSkjema.tsx
+++ src/frontend/komponenter/Fagsak/Forhåndsvarsel/ForhåndsvarselSkjema.tsx
@@ -21,6 +21,7 @@ import { BrevmalkodeEnum } from '../../../generated';
 import { forhåndsvisBrevMutation } from '../../../generated/@tanstack/react-query.gen';
 import { updateParentBounds } from '../../../utils/updateParentBounds';
 import { FixedAlert } from '../../Felleskomponenter/FixedAlert/FixedAlert';
+import PdfVisningModal from '../../Felleskomponenter/PdfVisningModal/PdfVisningModal';
 
 type Props = {
     behandling: BehandlingDto;
@@ -78,97 +79,108 @@ export const ForhåndsvarselSkjema: React.FC<Props> = ({
     }, []);
 
     return (
-        <div ref={containerRef}>
-            <HStack gap="4">
-                <ExpansionCard
-                    className="flex-1"
-                    aria-label={tittel}
-                    open={expansionCardÅpen}
-                    onToggle={setExpansionCardÅpen}
-                >
-                    <ExpansionCard.Header>
-                        <HStack>
-                            <ExpansionCard.Title size="small">{tittel}</ExpansionCard.Title>
-                        </HStack>
-                    </ExpansionCard.Header>
-                    <ExpansionCard.Content>
-                        <HStack align="center" justify="space-between">
-                            <Heading size="medium" level="2" spacing>
-                                {varselbrevtekster.overskrift}
+        <>
+            <div ref={containerRef}>
+                <HStack gap="4">
+                    <ExpansionCard
+                        className="flex-1"
+                        aria-label={tittel}
+                        open={expansionCardÅpen}
+                        onToggle={setExpansionCardÅpen}
+                    >
+                        <ExpansionCard.Header>
+                            <HStack>
+                                <ExpansionCard.Title size="small">{tittel}</ExpansionCard.Title>
+                            </HStack>
+                        </ExpansionCard.Header>
+                        <ExpansionCard.Content>
+                            <HStack align="center" justify="space-between">
+                                <Heading size="medium" level="2" spacing>
+                                    {varselbrevtekster.overskrift}
+                                </Heading>
+                                <Button
+                                    loading={seForhåndsvisningMutation.isPending}
+                                    icon={<FilePdfIcon aria-hidden />}
+                                    variant="tertiary"
+                                    onClick={seForhåndsvisning}
+                                >
+                                    Forhåndsvisning
+                                </Button>
+                            </HStack>
+                            <VStack maxWidth={ATextWidthMax}>
+                                {varselbrevtekster.avsnitter.map((avsnitt: Section) => (
+                                    <>
+                                        <div key={avsnitt.title}>
+                                            <Heading size="xsmall" level="3" spacing>
+                                                {avsnitt.title}
+                                            </Heading>
+                                            <BodyLong size="small" spacing>
+                                                {avsnitt.body}
+                                            </BodyLong>
+                                        </div>
+                                        {avsnitt.title === 'Dette har skjedd' && (
+                                            <form>
+                                                <Controller
+                                                    name="fritekst"
+                                                    control={control}
+                                                    rules={{
+                                                        required: 'Du må legge til en tekst',
+                                                        maxLength: {
+                                                            value: maksAntallTegn,
+                                                            message: `Maks ${maksAntallTegn} tegn`,
+                                                        },
+                                                    }}
+                                                    render={({ field }) => (
+                                                        <Textarea
+                                                            {...field}
+                                                            label="Legg til utdypende tekst"
+                                                            maxLength={maksAntallTegn}
+                                                            error={errors.fritekst?.message?.toString()}
+                                                            className="mb-6"
+                                                        />
+                                                    )}
+                                                />
+                                            </form>
+                                        )}
+                                    </>
+                                ))}
+                            </VStack>
+                        </ExpansionCard.Content>
+                    </ExpansionCard>
+                    {!expansionCardÅpen && (
+                        <Button
+                            loading={seForhåndsvisningMutation.isPending}
+                            icon={<FilePdfIcon aria-hidden />}
+                            variant="tertiary"
+                            onClick={seForhåndsvisning}
+                        >
+                            Forhåndsvisning
+                        </Button>
+                    )}
+                </HStack>
+                <div aria-live="assertive">
+                    {seForhåndsvisningMutation.error && (
+                        <FixedAlert
+                            variant="error"
+                            closeButton
+                            width={parentBounds.width}
+                            onClose={seForhåndsvisningMutation.reset}
+                        >
+                            <Heading spacing size="small" level="3">
+                                Forhåndsvisning feilet
                             </Heading>
-                            <Button
-                                icon={<FilePdfIcon aria-hidden />}
-                                variant="tertiary"
-                                onClick={seForhåndsvisning}
-                            >
-                                Forhåndsvisning
-                            </Button>
-                        </HStack>
-                        <VStack maxWidth={ATextWidthMax}>
-                            {varselbrevtekster.avsnitter.map((avsnitt: Section) => (
-                                <>
-                                    <div key={avsnitt.title}>
-                                        <Heading size="xsmall" level="3" spacing>
-                                            {avsnitt.title}
-                                        </Heading>
-                                        <BodyLong size="small" spacing>
-                                            {avsnitt.body}
-                                        </BodyLong>
-                                    </div>
-                                    {avsnitt.title === 'Dette har skjedd' && (
-                                        <form>
-                                            <Controller
-                                                name="fritekst"
-                                                control={control}
-                                                rules={{
-                                                    required: 'Du må legge til en tekst',
-                                                    maxLength: {
-                                                        value: maksAntallTegn,
-                                                        message: `Maks ${maksAntallTegn} tegn`,
-                                                    },
-                                                }}
-                                                render={({ field }) => (
-                                                    <Textarea
-                                                        {...field}
-                                                        label="Legg til utdypende tekst"
-                                                        maxLength={maksAntallTegn}
-                                                        error={errors.fritekst?.message?.toString()}
-                                                        className="mb-6"
-                                                    />
-                                                )}
-                                            />
-                                        </form>
-                                    )}
-                                </>
-                            ))}
-                        </VStack>
-                    </ExpansionCard.Content>
-                </ExpansionCard>
-                {!expansionCardÅpen && (
-                    <Button
-                        icon={<FilePdfIcon aria-hidden />}
-                        variant="tertiary"
-                        onClick={seForhåndsvisning}
-                    >
-                        Forhåndsvisning
-                    </Button>
-                )}
-            </HStack>
-            <div aria-live="assertive">
-                {seForhåndsvisningMutation.error && (
-                    <FixedAlert
-                        variant="error"
-                        closeButton
-                        width={parentBounds.width}
-                        onClose={seForhåndsvisningMutation.reset}
-                    >
-                        <Heading spacing size="small" level="3">
-                            Forhåndsvisning feilet
-                        </Heading>
-                        Kunne ikke forhåndsvise forhåndsvarselet. Prøv igjen senere.
-                    </FixedAlert>
-                )}
+                            Kunne ikke forhåndsvise forhåndsvarselet. Prøv igjen senere.
+                        </FixedAlert>
+                    )}
+                </div>
             </div>
-        </div>
+            {seForhåndsvisningMutation.isSuccess && (
+                <PdfVisningModal
+                    åpen={seForhåndsvisningMutation.isSuccess}
+                    pdfdata={seForhåndsvisningMutation.data}
+                    onRequestClose={() => seForhåndsvisningMutation.reset()}
+                />
+            )}
+        </>
     );
 };
