b49ec53e8410fe11f9e47f58db8d982d3c50839f
Fredrik Garseg Mørk <fredrik.1997@hotmail.com>
2025-12-19 13:25:03 +0100

    Controller rundt array feltet for å vise riktig feilmeldinger

--- src/frontend/komponenter/Fagsak/Forhåndsvarsel/skjema/UttalelseSkjema.tsx
+++ src/frontend/komponenter/Fagsak/Forhåndsvarsel/skjema/UttalelseSkjema.tsx
@@ -1,5 +1,5 @@
 import type { UttalelseMedFristFormData } from '../forhåndsvarselSchema';
-import type { FieldErrors, SubmitHandler } from 'react-hook-form';
+import type { SubmitHandler } from 'react-hook-form';
 
 import {
     VStack,
@@ -12,7 +12,7 @@ import {
 } from '@navikt/ds-react';
 import { ATextWidthMax } from '@navikt/ds-tokens/dist/tokens';
 import React from 'react';
-import { Controller, useFormContext, useWatch } from 'react-hook-form';
+import { Controller, get, useFormContext, useWatch } from 'react-hook-form';
 
 import { dateTilIsoDatoString } from '../../../../utils/dato';
 import { HarUttaltSeg } from '../forhåndsvarselSchema';
@@ -20,37 +20,6 @@ import { HarUttaltSeg } from '../forhåndsvarselSchema';
 type Props = {
     handleUttalelseSubmit: SubmitHandler<UttalelseMedFristFormData>;
     kanUtsetteFrist?: boolean;
-};
-
-type UttalelseJaErrors = FieldErrors<
-    Extract<UttalelseMedFristFormData, { harUttaltSeg: HarUttaltSeg.Ja }>
->;
-type UttalelseNeiErrors = FieldErrors<
-    Extract<UttalelseMedFristFormData, { harUttaltSeg: HarUttaltSeg.Nei }>
->;
-type UtsettFristErrors = FieldErrors<
-    Extract<UttalelseMedFristFormData, { harUttaltSeg: HarUttaltSeg.UtsettFrist }>
->;
-
-const isUttalelseJaErrors = (
-    _errors: FieldErrors<UttalelseMedFristFormData>,
-    harUttaltSeg: HarUttaltSeg | undefined
-): _errors is UttalelseJaErrors => {
-    return harUttaltSeg === HarUttaltSeg.Ja;
-};
-
-const isUttalelseNeiErrors = (
-    _errors: FieldErrors<UttalelseMedFristFormData>,
-    harUttaltSeg: HarUttaltSeg | undefined
-): _errors is UttalelseNeiErrors => {
-    return harUttaltSeg === HarUttaltSeg.Nei;
-};
-
-const isUtsettFristErrors = (
-    _errors: FieldErrors<UttalelseMedFristFormData>,
-    harUttaltSeg: HarUttaltSeg | undefined
-): _errors is UtsettFristErrors => {
-    return harUttaltSeg === HarUttaltSeg.UtsettFrist;
 };
 
 export const Uttalelse: React.FC<Props> = ({ handleUttalelseSubmit, kanUtsetteFrist = false }) => {
@@ -107,10 +76,10 @@ export const Uttalelse: React.FC<Props> = ({ handleUttalelseSubmit, kanUtsetteFr
                     </RadioGroup>
                 )}
             />
-            {harUttaltSeg === HarUttaltSeg.Ja && isUttalelseJaErrors(errors, harUttaltSeg) && (
+            {harUttaltSeg === HarUttaltSeg.Ja && (
                 <>
                     <Controller
-                        name={`uttalelsesDetaljer.${0}.uttalelsesdato`}
+                        name="uttalelsesDetaljer.0.uttalelsesdato"
                         control={methods.control}
                         render={({ field, fieldState }) => (
                             <DatePicker {...uttalelsesDatepicker.datepickerProps}>
@@ -123,58 +92,68 @@ export const Uttalelse: React.FC<Props> = ({ handleUttalelseSubmit, kanUtsetteFr
                             </DatePicker>
                         )}
                     />
-
-                    <TextField
-                        {...methods.register(`uttalelsesDetaljer.${0}.hvorBrukerenUttalteSeg`)}
-                        label="Hvordan uttalte brukeren seg?"
-                        description="For eksempel via telefon, Gosys, Ditt Nav eller Skriv til oss"
-                        error={errors.uttalelsesDetaljer?.[0]?.hvorBrukerenUttalteSeg?.message}
+                    <Controller
+                        name="uttalelsesDetaljer.0.hvorBrukerenUttalteSeg"
+                        control={methods.control}
+                        render={({ field, fieldState }) => (
+                            <TextField
+                                {...field}
+                                label="Hvordan uttalte brukeren seg?"
+                                description="For eksempel via telefon, Gosys, Ditt Nav eller Skriv til oss"
+                                error={fieldState.error?.message}
+                            />
+                        )}
                     />
-                    <Textarea
-                        {...methods.register(`uttalelsesDetaljer.${0}.uttalelseBeskrivelse`)}
-                        label="Beskriv hva brukeren har uttalt seg om"
-                        maxLength={4000}
-                        resize
-                        error={errors.uttalelsesDetaljer?.[0]?.uttalelseBeskrivelse?.message}
+                    <Controller
+                        name="uttalelsesDetaljer.0.uttalelseBeskrivelse"
+                        control={methods.control}
+                        render={({ field, fieldState }) => (
+                            <Textarea
+                                {...field}
+                                label="Beskriv hva brukeren har uttalt seg om"
+                                maxLength={4000}
+                                resize
+                                error={fieldState.error?.message}
+                            />
+                        )}
                     />
                 </>
             )}
 
-            {harUttaltSeg === HarUttaltSeg.Nei && isUttalelseNeiErrors(errors, harUttaltSeg) && (
+            {harUttaltSeg === HarUttaltSeg.Nei && (
                 <Textarea
                     {...methods.register('kommentar')}
                     label="Kommentar til valget over"
                     maxLength={4000}
                     resize
-                    error={errors.kommentar?.message}
+                    error={get(errors, 'kommentar.message')}
                 />
             )}
-            {harUttaltSeg === HarUttaltSeg.UtsettFrist &&
-                isUtsettFristErrors(errors, harUttaltSeg) && (
-                    <>
-                        <Controller
-                            name="utsettUttalelseFrist.nyFrist"
-                            control={methods.control}
-                            render={({ field, fieldState }) => (
-                                <DatePicker {...nyFristDatepicker.datepickerProps}>
-                                    <DatePicker.Input
-                                        {...nyFristDatepicker.inputProps}
-                                        label="Sett ny dato for frist"
-                                        name={field.name}
-                                        error={fieldState.error?.message}
-                                    />
-                                </DatePicker>
-                            )}
-                        />
-                        <Textarea
-                            {...methods.register('utsettUttalelseFrist.begrunnelse')}
-                            label="Begrunnelse for utsatt frist"
-                            maxLength={4000}
-                            resize
-                            error={errors.utsettUttalelseFrist?.begrunnelse?.message}
-                        />
-                    </>
-                )}
+            {harUttaltSeg === HarUttaltSeg.UtsettFrist && (
+                <>
+                    <Controller
+                        name="utsettUttalelseFrist.nyFrist"
+                        control={methods.control}
+                        render={({ field, fieldState }) => (
+                            <DatePicker {...nyFristDatepicker.datepickerProps}>
+                                <DatePicker.Input
+                                    {...nyFristDatepicker.inputProps}
+                                    label="Sett ny dato for frist"
+                                    name={field.name}
+                                    error={fieldState.error?.message}
+                                />
+                            </DatePicker>
+                        )}
+                    />
+                    <Textarea
+                        {...methods.register('utsettUttalelseFrist.begrunnelse')}
+                        label="Begrunnelse for utsatt frist"
+                        maxLength={4000}
+                        resize
+                        error={get(errors, 'utsettUttalelseFrist.begrunnelse.message')}
+                    />
+                </>
+            )}
         </VStack>
     );
 };
