6122fb9b398d8ad8ea32689660133cdc3bcb0971
Camilla Kaasi <camilla.lauritzen.kaasi@nav.no>
2026-01-26 12:07:43 +0100

    legger til designjusteringer

--- src/frontend/komponenter/Fagsak/Forhåndsvarsel/skjema/UttalelseSkjema.tsx
+++ src/frontend/komponenter/Fagsak/Forhåndsvarsel/skjema/UttalelseSkjema.tsx
@@ -80,9 +80,9 @@ export const Uttalelse: React.FC<Props> = ({
         <VStack
             as="form"
             maxWidth={ATextWidthMax}
-            gap="4"
-            onSubmit={methods.handleSubmit(handleUttalelseSubmit)}
+            className="gap-4.5"
             id="uttalelseForm"
+            onSubmit={methods.handleSubmit(handleUttalelseSubmit)}
         >
             <Controller
                 control={methods.control}
