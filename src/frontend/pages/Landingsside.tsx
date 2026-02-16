c7cae7038ff87627c78af78d16f64521f7974a13
Fredrik Garseg Mørk <fredrik.1997@hotmail.com>
2026-02-16 15:36:54 +0100

    Ferdigstilt landingsside

--- src/frontend/pages/Landingsside.tsx
+++ src/frontend/pages/Landingsside.tsx
@@ -1,23 +1,27 @@
 import { BodyLong, Heading } from '@navikt/ds-react';
 import * as React from 'react';
 
+import LandingssideImageUrl from '../images/dokumenter.svg';
+
 export const Landingsside: React.FC = () => (
-    <section className="flex justify-center items-center rounded-xl border-ax-bg-brand-blue-strong border bg-ax-bg-brand-blue-soft flex-col gap-4 py-13 max-w-223">
-        <img src="" alt=""></img>
+    <div className="flex items-center justify-center h-screen px-4">
+        <section className="flex items-center rounded-xl border-ax-bg-brand-blue-strong border bg-ax-bg-brand-blue-soft flex-col gap-4 py-13 w-full max-w-223 text-center">
+            <img src={LandingssideImageUrl} alt="Animert bilde av dokumenter" />
 
-        <Heading size="xlarge">Nav tilbakekreving</Heading>
+            <Heading size="xlarge">Nav tilbakekreving</Heading>
 
-        <BodyLong className="max-w-xl text-center flex flex-col gap-4">
-            <span className="font-semibold">
-                Velkommen til felles saksbehandlingsløsning for tilbakekreving.
-            </span>
+            <BodyLong className="max-w-xl flex flex-col gap-4">
+                <span className="font-semibold">
+                    Velkommen til felles saksbehandlingsløsning for tilbakekreving.
+                </span>
 
-            <span>
-                Dette gjelder ytelsene barnetrygd, kontantstøtte, enslig forsørger, tilleggsstønader
-                og arbeidsavklaringspenger. Flere ytelser vil komme senere.
-            </span>
+                <span>
+                    Dette gjelder ytelsene barnetrygd, kontantstøtte, enslig forsørger,
+                    tilleggsstønader og arbeidsavklaringspenger.
+                </span>
 
-            <span>Arbeidet med å utvide løsningen pågår, og flere ytelser vil kobles på.</span>
-        </BodyLong>
-    </section>
+                <span>Arbeidet med å utvide løsningen pågår, og flere ytelser vil kobles på.</span>
+            </BodyLong>
+        </section>
+    </div>
 );
