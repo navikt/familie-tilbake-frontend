54c989e912e8d7cb689f4f2e64ac81d2c71e2f47
Fredrik Garseg Mørk <fredrik.1997@hotmail.com>
2026-02-17 16:47:59 +0100

    Design ferdig

--- src/frontend/pages/feilsider/ikke-funnet.tsx
+++ src/frontend/pages/feilsider/ikke-funnet.tsx
@@ -1,12 +1,12 @@
-import { BodyShort, Box, Heading, Link as DsLink, Page, VStack } from '@navikt/ds-react';
+import { BugIcon } from '@navikt/aksel-icons';
+import { BodyShort, Box, Heading, Link, Page, VStack, List } from '@navikt/ds-react';
 import * as React from 'react';
-import { Link } from 'react-router';
 
 export const IkkeFunnet: React.FC = () => {
     return (
-        <Page.Block width="xl" gutters>
+        <Page.Block width="xl" gutters className="h-screen">
             <Box paddingBlock="space-80 space-64" data-aksel-template="404-v2">
-                <VStack gap="space-48" align="start">
+                <VStack gap="space-24" align="start">
                     <div>
                         <BodyShort textColor="subtle" size="small">
                             Statuskode 404
@@ -19,9 +19,23 @@ export const IkkeFunnet: React.FC = () => {
                             lenken.
                         </BodyShort>
                     </div>
-                    <DsLink as={Link} to="/">
-                        Gå til forsiden
-                    </DsLink>
+                    <VStack gap="space-8">
+                        <BodyShort className="font-semibold">Hva kan du gjøre?</BodyShort>
+                        <List>
+                            <List.Item>Prøve en annen lenke</List.Item>
+                            <List.Item>
+                                <Link href="/">Gå til forsiden</Link>
+                            </List.Item>
+                        </List>
+                    </VStack>
+                    <Link
+                        href="https://jira.adeo.no/plugins/servlet/desk/portal/541/create/6054"
+                        target="_blank"
+                        className="mt-2"
+                    >
+                        <BugIcon aria-hidden />
+                        Meld gjerne fra om at lenken ikke virker
+                    </Link>
                 </VStack>
             </Box>
         </Page.Block>
