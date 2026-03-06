a770e02509c73f964727df5ebad6ae3f4f458e1b
Fredrik Garseg Mørk <fredrik.1997@hotmail.com>
2026-03-06 16:24:24 +0100

    Retter opp i skeleton til forhåndsvarsel

--- src/frontend/pages/fagsak/forhåndsvarsel/ForhåndsvarselSkeleton.tsx
+++ src/frontend/pages/fagsak/forhåndsvarsel/ForhåndsvarselSkeleton.tsx
@@ -8,15 +8,15 @@ export const ForhåndsvarselSkeleton: FC = () => {
     return (
         <VStack className="gap-6">
             <Heading size="medium">Forhåndsvarsel</Heading>
-            <VStack className="max-w-xl">
-                <BodyShort className="text-ax-medium font-semibold">
+            <VStack className="max-w-xl" gap="space-2">
+                <BodyShort className="text-ax-medium font-semibold" size="small">
                     Skal det sendes forhåndsvarsel om tilbakekreving?
                 </BodyShort>
-                <BodyShort className="text-ax-text-neutral-subtle text-ax-medium" spacing>
+                <BodyShort className="text-ax-text-neutral-subtle text-ax-medium mb-3" size="small">
                     Brukeren skal som klar hovedregel varsles før vedtak om tilbakekreving fattes,
                     slik at de får mulighet til å uttale seg.
                 </BodyShort>
-                <VStack className="gap-1">
+                <VStack className="gap-3">
                     <Skeleton variant="rounded" width="10%" height={20} />
                     <Skeleton variant="rounded" width="10%" height={20} />
                 </VStack>
