e820f232c7aaec03b52286f69d71d5e20fdbe409
Fredrik Garseg Mørk <31205303+fredrikmork@users.noreply.github.com>
2026-05-04 13:45:37 +0200

    Foreldelse tekstendring (#2863)
    Endrer rekkefølge på begrunnelse og radiogroup
    Endrer på ordlyden slik at det blir tydeligere
    Small type på LocalAlert
    Bekreft-knapp justify-start

--- src/frontend/kodeverk/index.ts
+++ src/frontend/kodeverk/index.ts
@@ -1,6 +1,5 @@
 export * from './dokumentMal';
 export * from './fagsystem';
 export * from './rettsligGrunnlag';
-export * from './foreldelsevurdering';
 export * from './vedtak';
 export * from './vilkårsvurdering';
