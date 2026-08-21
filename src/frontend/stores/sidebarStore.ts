2d6422ecc949d9e4d04ebc40d2c20aac019f26b8
Fredrik Garseg Mørk <fredrik.1997@hotmail.com>
2026-08-21 11:40:49 +0200

    Fjerner ubrukt funksjon i store

--- src/frontend/stores/sidebarStore.ts
+++ src/frontend/stores/sidebarStore.ts
@@ -2,13 +2,11 @@ import { create } from 'zustand';
 
 export type SidebarState = {
     erÅpen: boolean;
-    setErÅpen: (erÅpen: boolean) => void;
     veksleÅpen: () => void;
 };
 
 export const useSidebarStore = create<SidebarState>(set => ({
     erÅpen: true,
-    setErÅpen: (erÅpen: boolean): void => set({ erÅpen }),
     veksleÅpen: (): void => set(state => ({ erÅpen: !state.erÅpen })),
 }));
 
