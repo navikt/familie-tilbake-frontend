690bb306391b9e74487cce668628eb5dea5b5ef1
Fredrik Garseg Mørk <fredrik.1997@hotmail.com>
2026-09-01 12:39:33 +0200

    Rettet foreldede sikkerhetsheader-config

--- src/backend/backend/headers.ts
+++ src/backend/backend/headers.ts
@@ -16,18 +16,14 @@ const setup = (app: Express): void => {
     app.disable('x-powered-by');
     app.use((_req, res, next) => {
         res.header('X-Frame-Options', 'DENY');
-        res.header('X-Xss-Protection', '1; mode=block');
         res.header('X-Content-Type-Options', 'nosniff');
         res.header('Referrer-Policy', 'no-referrer');
 
         if (process.env.NODE_ENV !== 'development') {
             res.header('Content-Security-Policy', cspString);
-            res.header('X-WebKit-CSP', cspString);
-            res.header('X-Content-Security-Policy', cspString);
         }
 
-        res.header('Feature-Policy', "geolocation 'none'; microphone 'none'; camera 'none'");
-        res.header('Access-Control-Allow-Methods', 'PUT, PATCH, GET, POST, DELETE');
+        res.header('Permissions-Policy', 'geolocation=(), microphone=(), camera=()');
         next();
     });
 };
