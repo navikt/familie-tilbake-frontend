6d6ef77659f904090d72528de9830be2463bb747
Camilla Kaasi <camilla.lauritzen.kaasi@nav.no>
2026-09-01 13:34:36 +0200

    Tillater rendring av pdf i dev og prod

--- src/backend/backend/headers.ts
+++ src/backend/backend/headers.ts
@@ -10,7 +10,7 @@ const navTelemetryDev = 'https://telemetry.ekstern.dev.nav.no';
 const sporingProxy = 'https://reops-event-proxy.nav.no';
 const sporingProxyDev = 'https://reops-event-proxy.ekstern.dev.nav.no';
 
-const cspString = `default-src 'self' data: ${amplitude} ${sentry} ${navTelemetry} ${navTelemetryDev} ${navFontSource} ${sporingProxy} ${sporingProxyDev}; style-src 'self' ${styleSource} data: 'unsafe-inline'; font-src 'self' ${fontSource} ${navFontSource} data:; frame-src 'self' blob:;`;
+const cspString = `default-src 'self' data: ${amplitude} ${sentry} ${navTelemetry} ${navTelemetryDev} ${navFontSource} ${sporingProxy} ${sporingProxyDev}; style-src 'self' ${styleSource} data: 'unsafe-inline'; font-src 'self' ${fontSource} ${navFontSource} data:; frame-src 'self' blob:; object-src 'self' blob:;`;
 
 const setup = (app: Express): void => {
     app.disable('x-powered-by');
